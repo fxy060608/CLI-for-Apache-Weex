686e3209f52cf54d9f8f38da4c4efb752870f43b
fxy060608 <fxy060608@gmail.com>
2019-06-26 22:05:51 +0800

    不在自动启用 chrome，屏蔽 network

--- packages/@weex/plugins/debug/src/index.js
+++ packages/@weex/plugins/debug/src/index.js
@@ -71,7 +71,7 @@ exports.launch = function (ip, port) {
       headless.launchHeadless(`${config.ip}:${config.port}`, open)
     })
   }
-  launcher.launchChrome(debuggerURL, config.REMOTE_DEBUG_PORT || 9222)
+  // launcher.launchChrome(debuggerURL, config.REMOTE_DEBUG_PORT || 9222)
 }
 
 exports.reload = function () {
